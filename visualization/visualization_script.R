30648274aee48dea2e3f9e32da5b2891dda603a8
tama-biro <samuel.thelaus@t-dab.com>
2022-08-03 12:21:40 +0100

    viz viz viz

--- visualization/visualization_script.R
+++ visualization/visualization_script.R
@@ -2,9 +2,81 @@
 library(tidyverse)
 library(lubridate)
 
+# Function to calculate standard error
+se = function(x) {
+  se = sd(x)/sqrt(length(x))
+  return(se)
+}
+
 df_sent <- read.csv('C:/Users/samue/Documents/GitHub/capstone_22/dataset/sent_downsampled.csv')
+df_topic_q <- read.csv('C:/Users/samue/Documents/GitHub/capstone_22/dataset/topic_q_downsampled.csv')
+df_topic_a <- read.csv('C:/Users/samue/Documents/GitHub/capstone_22/dataset/topic_a_downsampled.csv')
+
 
 # Number of texts per month
+
+# Average topic distribution
+
+# Questions
+df_topic_q_avg <- df_topic_q %>%
+  pivot_longer(c('topic_0', 'topic_1', 'topic_2',
+                 'topic_3', 'topic_4', 'topic_5'),
+               names_to="Topic",
+               values_to="Proportion") %>%
+  group_by(Topic) %>%
+  summarize(mean = mean(Proportion),
+            se = se(Proportion)) %>%
+  ungroup %>%
+  mutate(Topic = c('Crime', 'Trade', 'Contracts',
+                   'Inheritance', 'Payments', 'Real estate'))
+
+ggplot(df_topic_q_avg, aes(Topic, mean, fill = Topic)) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                width = 0.2) +
+  scale_y_continuous(expand = c(0, 0)) +
+  labs(y = 'Avg topic distribution') +
+  theme_minimal() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.y = element_text(size = 14),
+        axis.text.y = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 12))
+
+ggsave("C:/Users/samue/Documents/GitHub/capstone_22/plots/avg_topic_q.png",
+       width = 10, height = 6)
+
+
+# Answers
+df_topic_a_avg <- df_topic_a %>%
+  pivot_longer(c('topic_0', 'topic_1', 'topic_2',
+                 'topic_3', 'topic_4', 'topic_5'),
+               names_to="Topic",
+               values_to="Proportion") %>%
+  group_by(Topic) %>%
+  summarize(mean = mean(Proportion),
+            se = se(Proportion)) %>%
+  ungroup %>%
+  mutate(Topic = c('Co-living', 'Fines', 'Trade',
+                   'Contracts', 'Interpersonal', 'Inheritance'))
+
+ggplot(df_topic_a_avg, aes(Topic, mean, fill = Topic)) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                width = 0.2) +
+  scale_y_continuous(expand = c(0, 0)) +
+  labs(y = 'Avg topic distribution') +
+  theme_minimal() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.y = element_text(size = 14),
+        axis.text.y = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 12))
+
+ggsave("C:/Users/samue/Documents/GitHub/capstone_22/plots/avg_topic_a.png",
+       width = 10, height = 6)
 
 # Topic distribution by month for questions/answers
 
